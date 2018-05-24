01b9874bd9f42939e89dba0528041e8051fc9bc3
Joseph Papaluca <jpapaluc@gmail.com>
2018-05-24 12:54:59 -0400

    components

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -5,10 +5,10 @@ class ProductPage extends Component {
   render() {
     return (
       <div className="product-page-container">
-        <div className="product-section img">
+        <div className="product-section img-section">
           {/* image prop goes here */}
         </div>
-        <div className="product-section details">
+        <div className="product-section detail-section">
           <div className="product-header">
             <h2> Product Name </h2>
             <h3> $1000.99 </h3>
@@ -23,12 +23,22 @@ class ProductPage extends Component {
               </p>
             </div>
             <div className="product-info size">
-                {/* size component goes here */}
-                product size component
+              <h3 className="options-header"> Sizes: </h3>
+              <div className="product-options">
+                <div className="options-box"> S </div>
+                <div className="options-box"> M </div>
+                <div className="options-box"> L </div>
+                <div className="options-box"> XL </div>
+              </div>
             </div>
             <div className="product-info color">
-                {/* color component goes here */}
-                product color
+              <h3 className="options-header"> Colors: </h3>
+              <div className="product-options">
+                <div className="options-box white"> { /* white */ } </div>
+                <div className="options-box black"> { /* black */ } </div>
+                <div className="options-box red"> { /* red */ } </div>
+                <div className="options-box blue"> { /* blue */ } </div>
+              </div>
             </div>
             <div className="product-info checkout">
               <button className="btn product-btn"> Add to Cart. </button>
