14baff641527ec6acf2e3ab3415656e5e257d03e
Joseph Papaluca <jpapaluc@gmail.com>
2018-06-05 16:27:01 -0400

    container still won't pass prop state properly, headache remains

--- src/components/ProductContainer.js
+++ src/components/ProductContainer.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 // import NavHistory           from './NavHistory';
 import ProductIndex         from './ProductIndex';
-import ProductPage          from './ProductPage';
+// import ProductPage          from './ProductPage';
 
-import { products }         from './json/products.json';
+// import { products }         from './json/products.json';
 import                           './stylesheets/App.css';
 
 
 class ProductContainer extends Component {
 
   constructor() {
-    super();
+    super()
     this.state = { products: [] }
   }
 
+
   componentDidMount() {
-    let results = products.map
-    this.setState({ products: results })
     console.log(this.state);
+    // fetch("/json/products.json")
+    //   .then(res => res.json())
+    //   .then(products => this.setState({ products }))
   }
 
   render() {
--- src/components/ProductIndex.js
+++ src/components/ProductIndex.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-// import { products }         from './json/products.json';
+import { products }         from './json/products.json';
 
 class ProductIndex extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = { products: [] }
+  }
+
   render() {
     return (
-      <h2>product index</h2>
-    )
+      <div className="product-index">
+
+        <div className="index-container">
+
+          {
+            products.map(function(product) {
+
+              return    <div className="index-product" key={product.id}>
+                          <div className="index-img">
+                            {/* product thumbnail image goes here */}
+                          </div>
+                          <div className="index-details">
+                            <h3> <a href=""> {product.name} </a> </h3>
+                            <h4> {product.price} </h4>
+                          </div>
+                        </div>
+
+            })
+          }
+
+        </div>
+
+      </div>
+    );
   }
 }
 
-export default ProductIndex;
+export default ProductIndex
